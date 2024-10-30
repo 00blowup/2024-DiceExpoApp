8cbddc37cf34bbd12895fb63551979767d6b1509
Haeun Noh <00blowup@gmail.com>
2024-10-30 13:24:21 +0900

    practice: drawing 2 dices showing 9 dots and 6 dots each

--- app/index.tsx
+++ app/index.tsx
@@ -8,10 +8,47 @@ function Blank() {
     return <View style={ [st_circle, {backgroundColor: undefined, borderWidth: 0} ] } />;
 }
 
+var st_circle = {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'rgb(0, 200, 255)',
+    borderWidth: 1,
+    margin: 2
+};
+
+var st_dice = {
+    backgroundColor: 'rgb(255, 240, 200)',
+    padding: 10
+};
+
+function Dice() {
+    return (
+        <View style={st_dice}>
+            <View style={{flexDirection: 'row'}}><Circle/><Circle/><Circle/></View>
+            <View style={{flexDirection: 'row'}}><Circle/><Circle/><Circle/></View>
+            <View style={{flexDirection: 'row'}}><Circle/><Circle/><Circle/></View>
+        </View>
+    )
+}
+
+function Dice6() {
+    return (
+        <View style={st_dice}>
+            <View style={{flexDirection: 'row'}}><Circle/><Blank/><Circle/></View>
+            <View style={{flexDirection: 'row'}}><Circle/><Blank/><Circle/></View>
+            <View style={{flexDirection: 'row'}}><Circle/><Blank/><Circle/></View>
+        </View>
+    )
+}
+
+
 export default function Index() {
   return (
-    <View>
-
+    <View style={{flex:1, alignItems:'center'}}>
+        <Dice/>
+        <View style={{height:20}}/>
+        <Dice6/>
     </View>
   );
 }
